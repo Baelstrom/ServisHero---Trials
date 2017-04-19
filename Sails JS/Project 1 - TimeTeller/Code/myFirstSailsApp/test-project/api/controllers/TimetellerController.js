13fdca6991d187b1b41e95c8ff75f885e1cde9d0
Mohamed Ikhwan <1khwan.m0@gmail.com>
2017-04-19 19:13:50 +0530

    Still attempting to make user input time work.
    The majority of work is in the timeteller Controller where I'm attempting to process the  web api request.

--- Sails JS/Project 1 - TimeTeller/Code/myFirstSailsApp/test-project/api/controllers/TimetellerController.js
+++ Sails JS/Project 1 - TimeTeller/Code/myFirstSailsApp/test-project/api/controllers/TimetellerController.js
@@ -14,6 +14,7 @@ module.exports = {
     var year = d.getUTCFullYear();
 
     res.json({
+      datetime: d,
       day: day,
       month: month,
       year: year
@@ -23,11 +24,57 @@ module.exports = {
   },
 
   "dynamicdate" : function(req,res){
-
+    // redirects to the view where you're asked to input location
     res.view();
+  },
+
+  show: function (req,res,next){
+    var http = require('http');
+
+
+
+    // initial webservice request
+    function getCurrentTime(time, callback) {
+
+        // webapp link --> https://www.amdoren.com/api/timezone.php?api_key=428SsGhnM2NA4eUzt9KBYKdpksMgHQ&loc=Malaysia,Kuala+Lumpur
+
+      options = {
+        host: 'www.amdoren.com',
+        port: 80,
+        path: '/api/timezone.php?api_key=428SsGhnM2NA4eUzt9KBYKdpksMgHQ&loc=' +time.location,
+        method: 'GET'
+      };
+
+      // This is asynchronous - won't guarantee we get the response before we use it
+      var webservice_request = http.request(options, function(response) {
+        process_response(response, time, callback)
+      });
+      webservice_request.end();
+    } // END of getCurrentTime
+
+
+    // Handles the data that we get back
+    function process_response(webservice_response, time, callback) {
+      var webservice_data = "";
+      webservice_response.on('error', function(e) {
+        console.log(e.message);
+        callback("Error: " + e.message);
+      });
+      webservice_response.on('data', function(chunk) {
+        webservice_data += chunk;
+      });
+
+      webservice_response.on('end', function() {
+        var timeData = JSON.parse(webservice_data);
+        time.currentDateTime = timeData.time;
+        console.log(time.currentDateTime);
+        callback();
+      });
+    };
+
+
 
 
   }
-
 };
 
