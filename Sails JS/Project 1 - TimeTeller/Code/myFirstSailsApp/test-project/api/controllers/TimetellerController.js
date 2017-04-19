67569a0d48b4a89d4387535686019278d518fc04
Mohamed Ikhwan <1khwan.m0@gmail.com>
2017-04-19 16:41:29 +0530

    Created timeteller api

---
+++ Sails JS/Project 1 - TimeTeller/Code/myFirstSailsApp/test-project/api/controllers/TimetellerController.js
@@ -0,0 +1,11 @@
+/**
+ * TimetellerController
+ *
+ * @description :: Server-side logic for managing timetellers
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
